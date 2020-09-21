70bd416952a154bddbc7f650d082e7ee28e05726
Emily Chen <emseschen@gmail.com>
2020-09-20 22:18:45 -0400

    set up express server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+/*The first line gives you access to the express library by searching your node_modules for "express". The next creates an instance of the express constructor, which we will name "app".
+We can now access methods used for making a server by including their name after app.*/
+
+app.listen(8000,function(){
+console.log("server is running")
+})
+
+/*The app.listen method will start up the server locally on the port you give as its first argument (in this case the base url is: http://localhost:8000)*/
